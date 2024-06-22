d67c52acb80243109ad3aaa2a1bf2d6924514aba
Denis <denisdenis668@yahoo.com>
2024-06-23 02:09:54 +0300

    version 1.0beta

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './App.css';
+
+import Confeti from './components/Confeti.jsx';
+import Progressbar from '@ramonak/react-progress-bar';
+import Alesui from './assets/Alesui.png';
+
+const App = () => {
+  const completed = 1133;
+  const goal = 2500;
+  const percentage = (completed / goal) * 100;
+  return (
+    <div>
+      <Confeti />
+      <div className="text-white flex flex-col items-center justify-center">
+        <h1 className="text-5xl text-red-700 mb-4">Alesio sugus!!!</h1>
+        <img src={Alesui} alt="Alesio" className="w-auto h-64" />
+
+
+        <h1 className="text-4xl text-blue-300">Abonati care ma urmaresc!</h1>
+        <div className="flex flex-row items-center justify-center">
+          <progress className="w-96 h-4 bg-red-700 rounded-xl mr-5" value={percentage} max="100"></progress>
+          <h1 className="text-xl text-blue-500">{percentage.toFixed(2)}%</h1>
+        </div>
+        <h1 className="text-3xl text-yellow-500">1133/2500</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Confeti.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+
+import Confetti from 'react-confetti';
+import Gift from '../assets/gift.png';
+
+const Confeti = () => {
+    const { width, height } = useWindowSize();
+    return (
+        <div>
+            <div className="w-auto h-96 flex justify-center items-center"> 
+                <div className="flex flex-col items-center justify-center z-index-0">
+                    <h1 className="text-gray-100 bg-slate-800 p-3 rounded-md mb-2 text-sm">Ends joi 8th, 3088 @13:13am EST</h1>
+                    <h1 className="text-gray-100 uppercase bg-red-600 p-5 px-10 text-4xl font-bold pyramid-shape mb-4">Giveaway Lessi05yt</h1>
+
+                    <div className="flex flex-row items-center">
+                        {/* <h1>giftt</h1> */}
+                        <img src={Gift} alt="gift" className="w-32 h-32 mr-10"/>
+                        <h1 className="bg-green-700 text-2xl p-2 rounded-md rotate-[-10deg] font-bold">50$ CS2 SKIN</h1>
+                    </div>
+                </div>
+
+                
+                <Confetti
+                className="z-index-50" 
+                width={width} 
+                height={384}
+                numberOfPieces={100}
+                opacity={0.5}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Confeti;
